9f8a0cea12d199cd8b50e59425080c29484a89bb
Stoorx <antiproton2013@yandex.ru>
2021-05-03 18:05:09 +0300

    Piece of worse shit

--- RayTracerCs/Program.cs
+++ RayTracerCs/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
 
 namespace RayTracerCs
 {
@@ -6,7 +9,37 @@ namespace RayTracerCs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var camera = new Camera(new Vector3(0f, 0f, -1f), Quaternion.Identity, new Vector3(1f, 1f, 1f));
+            var lights = new LightSource(
+                new Vector3(0f, 1f, 1f),
+                Quaternion.Identity,
+                new List<Triangle>()
+                {
+                    new Triangle(new Vector3(-0.1f, 0.1f, 0f), new Vector3(0.1f, 0.1f, 0f),
+                        new Vector3(-0.1f, -0.1f, 0f)),
+                    new Triangle(new Vector3(0.1f, 0.1f, 0f), new Vector3(0.1f, -0.1f, 0f),
+                        new Vector3(-0.1f, -0.1f, 0f))
+                },
+                100,
+                SpectralColor.EqualEnergetic());
+            var objects = new MaterialObject(
+                new Vector3(0f, 0f, 1f),
+                Quaternion.Identity,
+                new List<Triangle>()
+                {
+                    new Triangle(new Vector3(-0.1f, 0.9f, 0f), new Vector3(0.1f, 0.1f, 0f),
+                        new Vector3(-0.1f, -0.1f, 0f)),
+                    new Triangle(new Vector3(0.1f, 0.1f, 0f), new Vector3(0.1f, -0.1f, 0.2f),
+                        new Vector3(-0.1f, -0.1f, 0f))
+                },
+                SpectralColor.One()
+            );
+            var scene = new Scene(new List<MaterialObject>() {objects}, new List<LightSource>() {lights});
+
+            var renderer = new Renderer(scene, camera);
+            var image = renderer.Render(100, 100);
+            var imgStr = HDRPrinter.StringifyImage(image);
+            File.WriteAllText("img.txt", imgStr);
         }
     }
 }
--- RayTracerCs/Scene.cs
+++ RayTracerCs/Scene.cs
@@ -4,6 +4,13 @@ namespace RayTracerCs
 {
     public class Scene
     {
+        public Scene(List<MaterialObject> objects, List<LightSource> lights)
+        {
+            Objects.AddRange(objects);
+            Objects.AddRange(lights);
+            Lights.AddRange(lights);
+        }
+
         public List<TracerObject> Objects { get; } = new();
         public List<LightSource> Lights { get; } = new();
     }
